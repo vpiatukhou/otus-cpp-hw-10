f6c084e335b66d22804dad3f50149830c509b3b1
Vasilij <v.piatukhou@gmail.com>
2023-02-25 12:04:35 +0300

    Fixed compilation error.

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
     }
 
     CommandProcessingContext commandContext;
-    context.init(blockSize);
+    commandContext.init(blockSize);
 
     startShutdownHandler(commandContext);
 
